9247a26e52ca5f9f4b8a83445f2c9db8ce2a48c7
Mário Monteiro <marioammonteiro@gmail.com>
2021-06-28 00:14:59 +0100

    added husky, excahnge env-cmd with dontenv, new launch config and other minimal changes, fix ts-node-dev vulnerabilties related with trim-newlines

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log(`env.MESSAGE: ${process.env.MESSAGE}...`);
+// init dotenv
+import {config} from 'dotenv';
+const result = config();
+if (result.error) {
+  throw result.error
+}
+
+console.log(`environment variable MESSAGE: ${process.env.MESSAGE}...`);
