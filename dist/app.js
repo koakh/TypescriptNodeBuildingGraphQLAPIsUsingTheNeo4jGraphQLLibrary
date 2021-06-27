3d42829cabac6ab0353dcaae3ed065ee7ee67d37
Mário Monteiro <marioammonteiro@gmail.com>
2021-06-28 00:24:29 +0100

    working on lint

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// init dotenv
+const dotenv_1 = require("dotenv");
+const result = dotenv_1.config();
+if (result.error) {
+    throw result.error;
+}
+debugger;
+console.log(`environment variable MESSAGE: ${process.env.MESSAGE}...`);
+//# sourceMappingURL=app.js.map
